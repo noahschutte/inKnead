54d0166906c6b3ab25f790f645b12ad539234c6b
FJ Collins Jr <fcollins@live.com>
2016-11-07 21:49:48 -0800

    style how to component to reflect designer. Add text for each step with image template for place holder.

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
 import {IndicatorViewPager, PagerTitleIndicator, PagerTabIndicator, PagerDotIndicator} from 'rn-viewpager';
 
 export default class Swiper extends Component {
@@ -11,15 +11,15 @@ export default class Swiper extends Component {
   }
   _renderTabIndicator() {
     let tabs = [{
-      text: 'Update Email',
+      text: 'Step 1',
       iconSource: require('../../assets/left_caret.png'),
       selectedIconSource: require('../../assets/left_caret.png')
     },{
-      text: 'Donate',
+      text: 'Step 2',
       iconSource: require('../../assets/left_caret.png'),
       selectedIconSource: require('../../assets/left_caret.png')
     },{
-      text: 'Request',
+      text: 'Step 3',
       iconSource: require('../../assets/left_caret.png'),
       selectedIconSource: require('../../assets/left_caret.png')
     }];
@@ -32,14 +32,23 @@ export default class Swiper extends Component {
           style={{flex:1}}
           indicator={this._renderTabIndicator()}
           >
-          <View style={{backgroundColor:'cadetblue'}}>
-            <Text>Update Email</Text>
+          <View style={styles.steps}>
+            <Text style={styles.text}>How It Works{"\n"}(Step 1)</Text>
+            <Image 
+              style={styles.donate} 
+              source={require('../../assets/donate.png')}
+            />
+            <Text style={styles.textBottom}>watch a video request,{"\n"}or make it your own</Text>
           </View>
-          <View style={{backgroundColor:'cornflowerblue'}}>
-            <Text>Donate</Text>
+          <View style={styles.steps}>
+            <Text style={styles.text}>How It Works{"\n"}(Step 2)</Text>
+            <Image style={styles.donate} source={require('../../assets/donate.png')}/>
+            <Text style={styles.textBottom}>choose what you want{"\n"}to donate, and receive{"\n"}donations</Text>
           </View>
-          <View style={{backgroundColor:'#1AA094'}}>
-            <Text>Request</Text>
+          <View style={styles.steps}>
+            <Text style={styles.text}>How It Works{"\n"}(Step 3)</Text>
+            <Image style={styles.donate} source={require('../../assets/donate.png')}/>
+            <Text style={styles.textBottom}>send a THANK YOU to{"\n"}the awesome person who{"\n"}just gave you pizza!</Text>
           </View>
         </IndicatorViewPager>
       </View>
@@ -83,4 +92,26 @@ const styles = StyleSheet.create({
     borderColor: 'orange',
     // borderWidth: 3,
   },
+  steps: {
+    backgroundColor:'white',
+    alignItems: 'center',
+    paddingTop: 60,
+    paddingBottom: 80,
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 30,
+    fontFamily: 'GillSans',
+    // need specific font, using this as template
+    textAlign: 'center'
+  },
+  textBottom: {
+    fontSize: 20,
+    fontFamily: 'GillSans',
+    textAlign: 'center'
+  },
+  donate: {
+    width: 200,
+    height: 100,
+  }
 })
