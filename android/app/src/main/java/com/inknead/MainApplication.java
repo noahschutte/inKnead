88bd1365f5c9e29e85e43bbd141a4f220acc8a2b
Jameson Welling <jamowelling@gmail.com>
2016-11-25 23:54:24 -0700

    migrate project to android studio / gradle environment

--- android/app/src/main/java/com/inknead/MainApplication.java
+++ android/app/src/main/java/com/inknead/MainApplication.java
@@ -5,12 +5,13 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.FacebookSdk;
-import com.facebook.AppEventsLogger;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTCameraPackage()
+          new RCTCameraPackage(),
+          new ReactVideoPackage()
       );
     }
   };
