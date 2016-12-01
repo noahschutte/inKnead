3fb9b9c66b1ce0af87156088f95c737933cb2c49
Jameson Welling <jamowelling@gmail.com>
2016-11-30 20:14:08 -0700

    fix typos in stylesheets

---
+++ src/components/style/InKneadStyle.js
@@ -0,0 +1,11 @@
+/*
+* @providesModule InKneadStyle
+*/
+
+import PlatformStyleSheet from './PlatformStyleSheet';
+
+const globalStyles = PlatformStyleSheet.create({
+  fontWeight: 500,
+});
+
+export default globalStyles;
--- src/components/style/PlatformStyleSheet.js
+++ src/components/style/PlatformStyleSheet.js
@@ -19,4 +19,4 @@ const PlatformStyleSheet= {
   },
 };
 
-export default PlatformStyleSheet
+export default PlatformStyleSheet;
