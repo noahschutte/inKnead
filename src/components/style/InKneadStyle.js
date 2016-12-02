8d00a4ef37543e60180bb496a4d0f800a2667875
Jameson Welling <jamowelling@gmail.com>
2016-12-02 15:12:04 -0700

    add some pizza image style to global styles

--- src/components/style/InKneadStyle.js
+++ src/components/style/InKneadStyle.js
@@ -7,53 +7,12 @@ import { StyleSheet } from 'react-native';
 import PlatformStyleSheet from 'PlatformStyleSheet';
 
 module.exports = PlatformStyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 2,
-    },
-    wrapper: {
-      flex: 1,
+    pizzas: {
       flexDirection: 'row',
-      borderBottomColor: '#BDBDBD',
-      borderTopColor: '#BDBDBD',
-      borderBottomWidth: 1,
-      borderTopWidth: 1,
-    },
-    imageContainer: {
-      flex: 1,
-      margin: 2,
-    },
-    thumbnail: {
-      resizeMode: 'contain',
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-    },
-    infoContainer: {
-      flex: 1,
-      margin: 5,
-    },
-    date: {
-      alignItems: 'flex-end',
-    },
-    dateTime: {
-      flex: 1,
-      textAlign: 'center',
-      fontSize: 10,
-      fontWeight: 'bold',
-    },
-    content: {
-      flex: 1,
-      flexDirection: 'row',
+      alignItems: 'flex-start',
     },
     pizzaImage: {
       height: 20,
       width: 20,
-    },
-    pizzas: {
-      flexDirection: 'row',
-      alignItems: 'flex-start',
     },
   })
