a36e5b9a6e5c90d08bc19de6edfbc72399444049
Jameson Welling <jamowelling@gmail.com>
2016-12-11 20:54:29 -0700

    get a buggy version of navbar-hiding up and running

--- android/app/src/main/java/com/inknead/MainActivity.java
+++ android/app/src/main/java/com/inknead/MainActivity.java
@@ -1,9 +1,13 @@
-package com.inKnead;
+package com.inknead;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
 import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,6 +15,33 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+     @Override
+     public void onWindowFocusChanged(boolean hasFocus) {
+       super.onWindowFocusChanged(hasFocus);
+
+       View decorView = getWindow().getDecorView();
+
+       if (hasFocus) {
+           decorView.setSystemUiVisibility(
+                      View.SYSTEM_UI_FLAG_LOW_PROFILE
+                  //  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  //  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  //  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  //  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  //  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                  //  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+           );
+       }
+    }
+
+    @Override
+    public void onStart() {
+      super.onStart();
+
+      View decorView = getWindow().getDecorView();
+
+      decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
