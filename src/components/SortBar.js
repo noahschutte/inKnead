6f78b49fc163cc41b87ffe11aa55eb232fd105f1
Jameson Welling <jamowelling@gmail.com>
2016-12-30 13:37:14 -0700

    create sort bar and sort button files for customizable sort bar

---
+++ src/components/SortBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class SortBar extends Component {
+  return (
+    <View style={styles.sortBarStyle}>      
+    </View>
+  )
+};
+
+const styles = {
+  sortBarStyle: {
+    flex: 0.5,
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginRight: 10,
+  }
+};
+
+export default SortBar;
