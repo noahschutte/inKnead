3cc3885f6faea09e4573dd820c4c5ed8f3eab7ac
Jameson Welling <jamowelling@gmail.com>
2016-12-30 13:38:12 -0700

    define forthcoming action types, add dispatch to getEntries action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
 import {
-  GET_ENTRIES_SUCCESS
+  GET_ENTRIES,
+  GET_ENTRIES_SUCCESS,
+  GET_USER_ENTRIES,
+  GET_USER_ENTRIES_SUCCESS,
 } from './types';
 
 export const getEntries = () => {
   return (dispatch) => {
+    dispatch({ type: GET_ENTRIES });
     fetch('https://d1dpbg9jbgrqy5.cloudfront.net/requests', {
       headers: {
         Accept: 'application/json',
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,4 @@
 export const GET_ENTRIES = 'get_requests_and_thank_yous';
 export const GET_ENTRIES_SUCCESS = 'get_entries_success';
+export const GET_USER_ENTRIES = 'get_user_entries';
+export const GET_USER_ENTRIES_SUCCESS = 'get_user_entries_success';
