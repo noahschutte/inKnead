6a5e88aa58e753a07f50ea8614b9cfacfd62b636
Jameson Welling <jamowelling@gmail.com>
2016-12-31 21:20:41 -0700

    refactor Loading Pizza into functional component rather than class component

--- src/components/LoadingPizza.js
+++ src/components/LoadingPizza.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import { Text, View, Image } from 'react-native';
 
-export default class LoadingPizza extends Component {
-
-  render() {
+const LoadingPizza = () => {
     return (
       <View style={{ flex: 1 }} >
-        <Image style={{ flex: 3, alignSelf: 'center', margin: 20, height: 75, resizeMode: 'contain' }} source={require('../../assets/mobile-icons/loadingPizza.png')} />
-        <Text style={{ flex: 5, textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>Loading...</Text>
+        <Image
+          style={{ flex: 3, alignSelf: 'center', margin: 20, height: 75, resizeMode: 'contain' }}
+          source={require('../../assets/mobile-icons/loadingPizza.png')}
+        />
+        <Text style={{ flex: 5, textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>
+          Loading...
+        </Text>
       </View>
     );
-  }
+  };
 
-}
+export default LoadingPizza;
