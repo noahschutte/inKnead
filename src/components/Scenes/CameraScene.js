0627569ae1dbf969a2331daf7ca55af6a9974f06
Jameson Welling <jamowelling@gmail.com>
2017-01-03 13:46:09 -0700

    add boilerplate for Scenes to be written down the road

---
+++ src/components/Scenes/CameraScene.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+
+class CameraScene extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default CameraScene;
---
+++ src/components/Scenes/NotificationsScene.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class NotificationsScene extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default NotificationsScene;
