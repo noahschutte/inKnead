0aa7099067037fde7b67ecfebc72cb37568f9bda
Jameson Welling <jamowelling@gmail.com>
2017-01-09 12:16:35 -0700

    create entry details boilerplate

---
+++ src/components/EntryDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EntryDetails extends Component {
+  render() {
+    return (
+      <View style={{ flex: 5 }}>
+        <Text>ENTRY DETAILS</Text>
+      </View>
+    );
+  }
+}
+
+export default EntryDetails;
