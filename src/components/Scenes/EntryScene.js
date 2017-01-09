9fa2c69e164b6241008c6e7aac58412114cfe8f2
Jameson Welling <jamowelling@gmail.com>
2017-01-09 12:16:03 -0700

    Entry Scene boilerplate

--- src/components/Scenes/EntryScene.js
+++ src/components/Scenes/EntryScene.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import NavBar from '../NavBar';
+import EntryVideo from '../EntryVideo';
+import EntryDetails from '../EntryDetails';
 
 class EntryScene extends Component {
   render() {
     return (
-      <View>
-        <Text>ENTRY SCENE</Text>
+      <View style={styles.container}>
+        <NavBar
+          leftButton='backButton'
+          onLeftPress={() => Actions.pop()}
+        />
+        <View style={{ flex: 9, backgroundColor: 'white' }}>
+          <EntryVideo
+            source={this.props.entry.compressed_video}
+          />
+          <EntryDetails />
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  container: {
+    flex: 1,
+  },
+  videoContainer: {
+    flex: 4,
+    // borderWidth: 2,
+    // borderColor: 'blue',
+  },
+  detailsContainer: {
+    flex: 5,
+    // borderWidth: 2,
+    // borderColor: 'green',
+  },
+};
 
 export default EntryScene;
