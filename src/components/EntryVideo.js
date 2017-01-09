fb2d5a93d6608a56559c7ed9194bb306619faebc
Jameson Welling <jamowelling@gmail.com>
2017-01-09 14:41:06 -0700

    stash dysfunctional version of EntryVideo

--- src/components/EntryVideo.js
+++ src/components/EntryVideo.js
@@ -1,35 +1,84 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
-import { connect } from 'react-redux';
+import { Text, Image, View, TouchableHighlight } from 'react-native';
 import Video from 'react-native-video';
-import { videoPlayToggle } from '../actions';
-import Button from './Button2';
 
 class EntryVideo extends Component {
+  componentWillReceiveProps() {
+    console.log('props received');
+  }
+
+  // playButtonHelper = () => {
+  //   const { paused, togglePlay } = this.props;
+  //   if (this.props.paused) {
+  //     return (
+  //       <View style={styles.buttonContainer}>
+  //         <Button onPress={() => togglePlay(paused)}>
+  //           <Image source={require('../../assets/mobile-icons/playbutton.png')} />
+  //         </Button>
+  //       </View>
+  //     );
+  //   }
+  //   return (
+  //     <TouchableHighlight style={styles.buttonContainer} onPress={() => console.log('here')}>
+  //       <Image source={require('../../assets/mobile-icons/playbutton.png')} />
+  //     </TouchableHighlight>
+  //   );
+  // }
+
   render() {
-    const { source, paused, videoPlayToggle } = this.props;
-    const playButton = (
-      <View style={styles.buttonContainer}>
-        <Button onPress={() => videoPlayToggle(paused)}>
-          <Image source={require('../../assets/mobile-icons/playbutton.png')} />
-        </Button>
-      </View>
-    );
+    console.log('rendered, this', this);
+    console.log('rendered, props', this.props);
+    const { source, paused, togglePlay } = this.props;
+    // let playButtonChild;
+    // if (paused) {
+    //   playButtonChild = <Image source={require('../../assets/mobile-icons/playbutton.png')} />;
+    // } else {
+    //   playButtonChild = <Image />;
+    // }
+    let playButton;
+    if (paused) {
+      playButton = (
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={require('../../assets/mobile-icons/playbutton.png')}
+          />
+      );
+    } else {
+      playButton = (
+        <Image style={{ width: 50, height: 50 }} />
+      );
+    }
+
+
     const videoDisplay = (
       <Video
         source={{ uri: source }}
         paused={paused}
-        muted
+        muted={false}
+        volume={1}
         playInBackground
         playWhenInactive
-        style={{ flex: 1, zIndex: 1 }}
+        style={{ flex: 1, zIndex: 2 }}
         resizeMode={'contain'}
+        onEnd={() => togglePlay(false)}
       />
     );
+
+    const display = (
+      <View style={{ flex: 1, zIndex: 1 }}>
+        <TouchableHighlight
+          style={styles.buttonContainer}
+          onPress={() => togglePlay(paused)}
+        >
+            {playButton}
+        </TouchableHighlight>
+        {videoDisplay}
+      </View>
+    );
+
     return (
       <View style={styles.container}>
-        {playButton}
-        {videoDisplay}
+        {display}
       </View>
     );
   }
@@ -38,10 +87,11 @@ class EntryVideo extends Component {
 const styles = {
   container: {
     flex: 4,
+    zIndex: 1
   },
   video: {
-    borderColor: 'black',
-    borderWidth: 2,
+    // borderColor: 'black',
+    // borderWidth: 2,
   },
   buttonContainer: {
     position: 'absolute',
@@ -52,12 +102,9 @@ const styles = {
     left: 0,
     right: 0,
     zIndex: 3,
-  }
+    // borderColor: 'red',
+    // borderWidth: 3,
+  },
 };
 
-const mapStateToProps = state => {
-  const { paused } = state.video;
-  return { paused };
-};
-
-export default connect(mapStateToProps, { videoPlayToggle })(EntryVideo);
+export default EntryVideo;
