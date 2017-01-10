5ad7b32c76b30ea539473bb7b06c4ba9f6507925
Jameson Welling <jamowelling@gmail.com>
2017-01-09 21:55:56 -0700

    allow for Request pizzas to differentiate between large and small sizes

--- src/components/RequestPizzas.js
+++ src/components/RequestPizzas.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 
-const RequestPizzas = (props) => {
+const RequestPizzas = ({ pizzas, size }) => {
   const pizzaImages = [];
-  for (let i = 0; i < props.pizzas; i++) {
+  let style;
+  switch (size) {
+    case 'large':
+      style = styles.pizzaImageLarge;
+      break;
+    default:
+      style = styles.pizzaImageSmall;
+  }
+  for (let i = 0; i < pizzas; i++) {
     const pizzaImage = (
       <Image
-        style={styles.pizzaImage}
+        style={style}
         source={require('../../assets/pizza-icon-for-requests/whole-pizza.png')}
         key={i}
       />
@@ -22,9 +30,13 @@ const RequestPizzas = (props) => {
 };
 
 const styles = {
-  pizzaImage: {
+  pizzaImageSmall: {
     width: 20,
     height: 20,
+  },
+  pizzaImageLarge: {
+    width: 50,
+    height: 50,
   },
   pizzas: {
     flexDirection: 'row',
