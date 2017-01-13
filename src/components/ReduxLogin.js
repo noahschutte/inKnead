d446af5208b7597bcc806b58b44d236b5149e88c
Jameson Welling <jamowelling@gmail.com>
2017-01-12 19:45:06 -0700

    add login capabilities with redux-modifying state

---
+++ src/components/ReduxLogin.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import {
+  AccessToken,
+  LoginButton,
+  GraphRequest,
+  GraphRequestManager
+} from 'react-native-fbsdk';
+import { connect } from 'react-redux';
+import { userLogout, createSession } from '../actions';
+
+class ReduxLogin extends Component {
+  handleLogoutFinished = (error, result) => {
+    if (error) {
+      alert(`Logout failed with error: ${result.error}`);
+    } else {
+      this.props.userLogout();
+    }
+  }
+  handleLoginFinished = (error, result) => {
+    if (error) {
+      alert(`Login failed with error ${error}`);
+    } else if (result.isCancelled) {
+      alert('Login was cancelled');
+    } else {
+      AccessToken.getCurrentAccessToken().then(
+        data => {
+          if (data) {
+            const accessToken = data.accessToken;
+            const responseInfoCallback = (error, result) => {
+              if (error) {
+                alert(`Error fetching data: ${error.toString()}`);
+              } else {
+                this.props.createSession(result);
+              }
+            };
+            const infoRequest = new GraphRequest(
+              '/me',
+              {
+                accessToken,
+                parameters: {
+                  fields: {
+                    string: 'email,name,first_name,middle_name,last_name'
+                  }
+                }
+              },
+              responseInfoCallback
+            );
+            new GraphRequestManager().addRequest(infoRequest).start();
+          }
+        }
+      );
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <LoginButton
+          readPermissions={['email', 'public_profile']}
+          onLogoutFinished={this.handleLogoutFinished}
+          onLoginFinished={this.handleLoginFinished}
+        />
+      </View>
+    );
+  }
+}
+
+export default connect(null, { userLogout, createSession })(ReduxLogin);
