274fd1806014eb7acc77f9bdd25806a55078f1df
Jameson Welling <jamowelling@gmail.com>
2017-01-14 13:34:10 -0700

    add method to be invoked upon video record button press

--- src/components/Scenes/EntryCreationScene.js
+++ src/components/Scenes/EntryCreationScene.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Platform } from 'react-native';
+import Camera from 'react-native-camera';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import {
@@ -13,12 +14,27 @@ import EntryCreationForm from '../EntryCreationForm';
 import Button from '../Button2';
 
 class EntryCreationScene extends Component {
+  openVideoRec = () => {
+    if (Platform.OS === 'ios') {
+      Camera.checkDeviceAuthorizationStatus()
+      .then(response => {
+        if (response) {
+          Actions.CameraScene();
+        } else {
+          alert('You must allow camera access!');
+        }
+      });
+    } else if (Platform.OS === 'android') {
+      Actions.CameraScene();
+    }
+  }
+
   renderVideoContent = () => {
     if (this.props.videoData) {
       return <Text>Video Data exists</Text>;
     }
     return (
-      <Button>
+      <Button onPress={this.openVideoRec}>
         <Image
           source={camcorderImage}
           style={{ resizeMode: 'contain', height: 75, width: 75 }}
