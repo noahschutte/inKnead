ea903c58a690788d44d4fe91d88da536cdf22452
Jameson Welling <jamowelling@gmail.com>
2017-01-14 13:33:41 -0700

    style placeholder text

--- src/components/Scenes/CameraScene.js
+++ src/components/Scenes/CameraScene.js
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 class CameraScene extends Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>CAMERA SCENE</Text>
       </View>
     );
