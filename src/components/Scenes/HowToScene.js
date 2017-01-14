f156d699117d638ad7c0f4ccdd47accef7e3e57a
Jameson Welling <jamowelling@gmail.com>
2017-01-14 12:49:22 -0700

    rebuild and incorporate How To Scene(s)

--- src/components/Scenes/HowToScene.js
+++ src/components/Scenes/HowToScene.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import NavBar from '../NavBar';
+import HowToSwiper from '../HowToSwiper';
 
 class HowToScene extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
         <NavBar
-          title='How-To'
           leftButton='backButton'
           onLeftPress={Actions.pop}
         />
         <View style={{ flex: 9 }}>
-          <Text>HOW TO SCENE</Text>
+          <HowToSwiper />
         </View>
       </View>
     );
