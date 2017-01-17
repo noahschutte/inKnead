cd9c76498e41ebcebf9c182e3576a51e27a82cf7
Jameson Welling <jamowelling@gmail.com>
2017-01-16 23:57:18 -0700

    refactor HowTo from class to function

--- src/components/Scenes/HowToScene.js
+++ src/components/Scenes/HowToScene.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import NavBar from '../NavBar';
 import HowToSwiper from '../HowToSwiper';
 
-class HowToScene extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <NavBar
-          leftButton='backButton'
-          onLeftPress={Actions.pop}
-        />
-        <View style={{ flex: 9 }}>
-          <HowToSwiper />
-        </View>
+const HowToScene = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <NavBar
+        leftButton='backButton'
+        onLeftPress={Actions.pop}
+      />
+      <View style={{ flex: 9 }}>
+        <HowToSwiper />
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 export default HowToScene;
