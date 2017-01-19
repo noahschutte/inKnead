ff4e9189394799685a559d8edc3d07c69858e10d
Jameson Welling <jamowelling@gmail.com>
2017-01-18 21:50:22 -0700

    make loading pizza spin with Animated.Image

--- src/components/LoadingPizza.js
+++ src/components/LoadingPizza.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Image, Animated, Easing } from 'react-native';
 import { loadingPizzaImage } from '../assets';
 
-const LoadingPizza = () => {
+class LoadingPizza extends Component {
+  constructor(props) {
+    super(props);
+    this.spinValue = new Animated.Value(0);
+  }
+  componentDidMount() {
+    this.spin();
+  }
+  spin = () => {
+    this.spinValue.setValue(0);
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear,
+      }
+    ).start(this.spin);
+  }
+  render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
     return (
       <View style={{ flex: 1 }} >
-        <Image
-          style={{ flex: 3, alignSelf: 'center', margin: 20, height: 75, resizeMode: 'contain' }}
-          source={loadingPizzaImage}
-        />
-        <Text style={{ flex: 5, textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>
-          Loading...
-        </Text>
+          <Animated.Image
+            style={{ flex: 3, transform: [{ rotate: spin }], alignSelf: 'center', margin: 20, height: 75, resizeMode: 'contain' }}
+            source={loadingPizzaImage}
+          />
+          <Text style={{ flex: 5, textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>
+            Loading...
+          </Text>
       </View>
     );
-  };
+  }
+}
 
 export default LoadingPizza;
