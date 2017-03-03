dd01f92585646d82f245dcf8b9997f31834fa104
Jameson Welling <jamowelling@gmail.com>
2017-03-03 14:28:01 -0700

    refactor pizza radio button to functional component

--- src/components/PizzaRadioButton.js
+++ src/components/PizzaRadioButton.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, TouchableWithoutFeedback } from 'react-native';
 
-class PizzaRadioButton extends Component {
-  assembleImages = () => {
-    const { options, selectedOption, selectedImage, unselectedImage } = this.props;
-    const content = [];
-    for (const option of options) {
-      if (option > selectedOption) {
-        content.push(unselectedImage);
-      } else {
-        content.push(selectedImage);
-      }
+const PizzaRadioButton = props => {
+  const {
+    options,
+    selectedOption,
+    selectedImage,
+    unselectedImage,
+    onPress
+  } = props;
+
+  const content = [];
+  for (const option of options) {
+    if (option > selectedOption) {
+      content.push(unselectedImage);
+    } else {
+      content.push(selectedImage);
     }
-    return content;
   }
-  renderContent = () => {
-    const onPress = this.props.onPress;
-    const images = this.assembleImages();
-    return (
-      <View style={styles.container}>
-        {images.map((image, index) => {
-          return (
-            <TouchableWithoutFeedback onPress={() => onPress(index + 1)} key={index}>
-              {image}
-            </TouchableWithoutFeedback>
-          );
-        })}
-      </View>
+
+  return (
+    <View style={styles.container}>
+      {content.map((image, index) => {
+        return (
+          <TouchableWithoutFeedback onPress={() => onPress(index + 1)} key={index}>
+            {image}
+          </TouchableWithoutFeedback>
+        );
+      })}
+    </View>
     );
-  }
-  render() {
-    const content = this.renderContent();
-    return content;
-  }
-}
+};
 
 const styles = {
   container: {
